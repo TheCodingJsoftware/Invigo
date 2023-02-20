bd14dff6843143f5a8135e75b7b3f7e5a267ade6
JareBear <jaredgrozz@gmail.com>
2023-02-20 12:24:08 -0600

    server

--- server/server.py
+++ server/server.py
@@ -23,7 +23,7 @@ class Server:
         logs and starts the server
         """
         # Declaring server IP and port
-        self.SERVER_IP: str = "10.0.0.217"
+        self.SERVER_IP: str = "10.0.0.211"
         self.SERVER_PORT: int = 4000
 
         self.BUFFER_SIZE = 8096
