68f12c381ce9fd2f4b999a02a78a49a845e444eb
JareBear <jaredgrozz@gmail.com>
2023-02-20 09:24:47 -0600

    server

--- server/server.py
+++ server/server.py
@@ -26,7 +26,7 @@ class Server:
         self.SERVER_IP: str = "10.0.0.237"
         self.SERVER_PORT: int = 4000
 
-        self.BUFFER_SIZE = 1500
+        self.BUFFER_SIZE = 1024
         self.SEPARATOR = "<SEPARATOR>"
 
         self.check_folders(folders=["data", "logs", "backups"])
