ad4867abda90589bc012a7a63d191b84b424510e
JareBear <jaredgrozz@gmail.com>
2023-02-21 14:40:53 -0600

    server

--- server/server.py
+++ server/server.py
@@ -176,6 +176,8 @@ class Server:
                             else:
                                 # file transmitting is done
                                 break
+                    # receive acknowledgment from the server
+                    _ = client_socket.recv(1024)
                     logging.info("sent response")
                     print(
                         f"{Colors.BOLD}{datetime.now()}{Colors.ENDC}\t\t{Colors.OKGREEN}[+] Succesfully received file{Colors.ENDC}"
@@ -204,7 +206,7 @@ class Server:
                         logging.info(
                             f"Error loading file, improper JSON format, aborting. {e}"
                         )
-                    client_socket.send("Batch sent successfully".encode("utf-8"))
+                    client_socket.sendall("Batch sent successfully".encode("utf-8"))
                 print(
                     f"{Colors.BOLD}{datetime.now()}{Colors.ENDC}\t{Colors.OKGREEN}[+] Process finished!{Colors.ENDC}"
                 )
