278031a1a8a6c7c16ef3380f7ffca57bd65b37e5
JareBear <jaredgrozz@gmail.com>
2024-05-28 15:37:04 -0500

    Moved server to: https://github.com/TheCodingJsoftware/Invigo-Server

--- server/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\Invigo\Inventory-Manager\server
-C:\Users\Invigo\AppData\Local\Programs\Python\Python310\python.exe server.py %*
--- server/utils/inventory/category.py
+++
@@ -1,6 +0,0 @@
-class Category:
-    def __init__(self, name: str):
-        self.name = name
-
-    def rename(self, new_name: str):
-        self.name = new_name
