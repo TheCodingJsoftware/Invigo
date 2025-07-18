f0be24e5f9b48be3c2ec47a20582ea81b87e45fa
Jared Gross <jaredgrozz@gmail.com>
2025-07-17 20:45:46 -0500

    feat: add UpdateDialog and UI for displaying update information and notes

---
+++ ui/dialogs/update_dialog.py
@@ -0,0 +1,31 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtWidgets import QDialog
+
+from ui.dialogs.update_dialog_UI import Ui_Form
+from ui.icons import Icons
+
+
+class UpdateDialog(QDialog, Ui_Form):
+    def __init__(self, parent, current_version: str, new_version: str, update_notes: str):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.parent = parent
+
+        self.setWindowTitle("New Update Available")
+        self.setWindowIcon(QIcon(Icons.invigo_icon))
+        self.setFixedSize(600, 500)
+
+        pixmap = QPixmap(Icons.invigo_icon)
+        scaled_pixmap = pixmap.scaled(self.label_icon.size(), Qt.AspectRatioMode.KeepAspectRatio)
+
+        self.label_icon.setFixedSize(128, 128)
+        self.label_icon.setPixmap(scaled_pixmap)
+
+        self.label_version.setText(f"Current Version: {current_version}\nNew Version: {new_version}")
+        self.label_update_notes.setText(update_notes)
+
+        self.scrollArea.setStyleSheet("border: 0px")
+
+        self.pushButton_update.clicked.connect(self.accept)
+        self.pushButton_cancel.clicked.connect(self.reject)
