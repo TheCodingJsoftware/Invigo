137303fac078eefaac24c2615bcf9b009c9cfbaf
Jared Gross <jaredgrozz@gmail.com>
2025-07-21 14:41:18 -0500

    chore: bump version to v4.0.4

--- ui/windows/main_window.py
+++ ui/windows/main_window.py
@@ -165,7 +165,7 @@ from utils.workspace.workspace import Workspace
 from utils.workspace.workspace_laser_cut_part_group import WorkspaceLaserCutPartGroup
 from utils.workspace.workspace_settings import WorkspaceSettings
 
-__version__: str = "v4.0.3"
+__version__: str = "v4.0.4"
 
 
 def check_folders(folders: list[str]):
