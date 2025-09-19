0f7f1d3e36a944af58750b6573df6f685c3a40e8
Jared Gross <jaredgrozz@gmail.com>
2025-09-19 17:23:07 -0500

    v4.0.32

--- ui/windows/main_window.py
+++ ui/windows/main_window.py
@@ -157,7 +157,7 @@ from utils.workspace.workspace import Workspace
 from utils.workspace.workspace_laser_cut_part_group import WorkspaceLaserCutPartGroup
 from utils.workspace.workspace_settings import WorkspaceSettings
 
-__version__: str = "v4.0.31"
+__version__: str = "v4.0.32"
 
 
 def check_folders(folders: list[str]):
